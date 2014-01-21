64b49dc9c2d25e89ca8baaa17f800d07201c52ea
ayu-mushi <ayumu.kawasaki3@gmail.com>
2014-01-21 22:05:29 +0900

    引数の数かぞえるの面倒くさかったから全部カリー化した

--- primitive.js
+++ primitive.js
@@ -1,20 +1,10 @@
 function getElm(a,i){return a[i]}
-function call_(f,ctx,a,b){return f.call(ctx,a,b)}
+function pushCtx(f){return function(ctx,args){return f.apply(ctx,args)}}
 function flip(f){return function(b,a){return f(a,b)}}
+function cflip(f){return function(b,a){return f(b)(a)}}
 function fillArr(e,len){var a=[];for(var i=len-1;i+1;i--)a[i]=e;return a}
-function curry(f,interval){
-	return function _(args,i){
-		return i<interval.length?
-			function(){return _(args.concat([].slice.call(arguments,0,interval[i])),i+1)}:f.apply(null,args)
-	}([],0)
-}
-/*function curryAll(f,interval){
-	return
-		fillArr(function(a){return function},interval.length).concat(function(a){return f.apply(null,a)})
-			.reduce(function(f,g){return })
-}*/
-function partial(f,len,args){return curry(f,[args.length,len-args.length]).apply(null,args)}
-function composeMul(f,g,fLen){return function(){return f.apply(null,[g.apply(null,[].slice.call(arguments,0,fLen))],[].slice.call(arguments,fLen))}}
+function curry(f){return function _(args,i){return i?function(a){return _(args.concat([a]),i-1)}:f.apply(null,args)}([],f.length)}
+function compose(f,g){return function(){return f(g.apply(null,arguments))}}
 function id(a){return a}
 function iif(p,a,b){return p?a:b}
 function eq(a,b){return a==b}
@@ -24,6 +14,10 @@ function sub(x,y){return x-y}
 function mul(x,y){return x*y}
 function div(x,y){return x/y}
 function mod(x,y){return x%y}
-const pushCtx=curry(call_,[1,3]),
-flippar=composeMul(partial,flip),
-fpow=composeMul(fillArr,flippar(pushCtx([].reduce),composeMul),2)
+const inc=curry(inc)(1),
+dec=curry(flip(sub))(1),
+makeArr=curry(fillArr)(0),
+uncurry1=compose(cflip,cflip),
+reduce_=curry(flip(pushCtx([].reduce))),
+uncurryAll=compose(reduce_,uncurry1),
+fpow=[].reduce,compose,fillArr

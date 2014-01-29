9bcaefdb85951b2f624189d753eb0bfc3a3b4617
ayu-mushi <ayumu.kawasaki3@gmail.com>
2014-01-29 23:41:45 +0900

    名前

--- primitive.js
+++ primitive.js
@@ -1,5 +1,5 @@
 function iif(p,a,b){return p?a:b}
-function eq(a,b){return a==b}
+function eq(a,b){return a===b}
 function opposite(x){return -x}
 function add(x,y){return x+y}
 function sub(x,y){return x-y}
@@ -11,32 +11,32 @@ function fillArr(e,len){var a=[];for(var i=len-1;i+1;i--)a[i]=e;return a}
 function id(a){return a}
 function args(f){return function(){return f.apply(null,[].slice.call(arguments,0,f.length),[[].slice.call(arguments,f.length)])}}
 function curry(f){return args(function(a){return args(function(b){return f.apply(null,a,b)})})}
-const apply_ctx=curry(args(function(f,ctx,a){return f.apply(ctx,a)})),
+const appctx=curry(args(function(f,ctx,a){return f.apply(ctx,a)})),
 flip=curry(args(function(f,a,b,rest){return f.apply(null,[b],[a],rest)})),
-constant=curry(id),
 uncurry1=curry(function(f,a,b){return f(a)(b)}),
 compose=curry(args(function(f,g,a){return f(g.apply(null,a))})),
+constant=curry(id),
 partial=uncurry1(curry),
-flipCurry=compose(curry,flip),
-flippar=uncurry1(flipCurry),
-apply_=flippar(apply_ctx,null),
-wCombir=compose(curry(apply_ctx),fillArr),
-apply_ctxdMap=apply_ctx([].map),
-apply_ctxdReduce=apply_ctx([].reduce),
-pam=compose(flipCurry(apply_ctxdMap),flipCurry(apply_)),
-itrate=pam,
+curry2nd=compose(curry,flip),
+bind2nd=uncurry1(curry2nd),
+apply_=bind2nd(appctx,null),
+wCombir=compose(curry(appctx),fillArr),
+map_=curry2nd(appctx([].map)),
+reduce_=curry2nd(appctx([].reduce)),
+pam=compose(map_,curry2nd(apply_)),
+itrate,
 hook,
-fork=compose(),
+fork,
 train,
-uncurryAll=compose(flipCurry(curry([].reduce)),uncurry1),
-fpow=compose(flipCurry([].reduce)(compose),fillArr),
+uncurryAll=reduce_(uncurry1),
+fpow=compose(reduce_(compose),fillArr),
 curryN=partial(fpow,curry),
 inc=partial(add,1),
 dec=partial(add,-1),
-mapObj=compose(flipCurry(curry([].map))(),{}.keys),//function(f,a){return Object.keys(a).map(function(i){return f(a[i],i,a)})}
+mapObj=compose(map_(),{}.keys),//function(f,a){return Object.keys(a).map(function(i){return f(a[i],i,a)})}
 merge,
-head=flippar(get,0),
-last=ctx(ctx([].slice)),
+head=bind2nd(get,0),
+last=appctx([].slice),
 tail,
 init,
 zip,

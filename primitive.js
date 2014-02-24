e18a360ff3af4f5b91c4ec718f43e732c0cc5e87
ayu-mushi <ayumu.kawasaki3@gmail.com>
2014-02-25 00:57:49 +0900

    fpowをy使わずにメモ化対応にするには、fpowを一般化したような forを関数にしたようなのを、つくればいいかな

--- primitive.js
+++ primitive.js
@@ -1,23 +1,6 @@
 function iif(p,a,b){return p?a:b}
-function eq(a,b){return a===b}
-function neq(a,b){return a!==b}
-function not(p){return !p}
-function and(p,q){return p&&q}
-function or(p,q){return p||q}
-function neg(n){return -n}
-function add(n,m){return n+m}
-function sub(n,m){return n-m}
-function mul(n,m){return n*m}
-function div(n,m){return n/m}
-function mod(n,m){return n%m}
-function lt(n,m){return n<m}
-function gt(n,m){return n>m}
-function le(n,m){return n<=m}
-function ge(n,m){return n>=m}
 function get(a,i){return a[i]}
 function set(a,i,e){a[i]=e;return a}
-function fillArr(e,len){var a=[];for(var i=len-1;i+1;i--)a[i]=e;return a}
-function id(a){return a}
 function apply_(f,ctx,a){return f.apply(ctx,a)}
 function args(f,n){
 	return function(){
@@ -34,12 +17,17 @@ function curry(f){
 const flip=curry(args(function(f,a,b,rest){return f.apply(null,[b].concat([a],rest))},3)),
 uncurry1=curry(function(f,a,b){return f(a)(b)}),
 compose=curry(args(function(f,g,a){return f(g.apply(null,a))},2)),
-fpow=curry(args(function(f,n,a){for(;n+1;n--)a=f(a,n);return a},2)),
+fpow=curry(function(f,n,a){for(;n;n--)a=f(a,n);return a}),
 call_=curry(args(apply_,2)),
-constant=curry(id),
 partial=uncurry1(curry),
 curry2nd=compose(curry,flip),
 bind2nd=uncurry1(curry2nd),
+op1=compose(partial(Function,"a"),bind2nd(call_("".concat,"return "),"a")),
+op2=compose(partial(Function,"a,b"),bind2nd(call_("".concat,"return a"),"b")),
+op={},
+id=op1(""),
+constant=curry(id),
+fillArr,
 justapp=bind2nd(apply_,null),
 wcate=compose(curry2nd(justapp),fillArr),
 pass=compose(curry2nd(justapp),bind2nd(call_([].slice),0)),
@@ -48,19 +36,19 @@ itrate,
 hook,
 fork,
 train,
-uncurry=args(bind2nd(call_([].reduce),uncurry1),0),
-//fpow=compose(bind2nd(call_([].reduce),pass(compose,2)),fillArr),
-inc=partial(add,1),
-dec=partial(add,-1),
+uncurryAll=args(bind2nd(call_([].reduce),uncurry1),0),
+inc=partial(op2("+"),1),
+dec=partial(op2("+"),-1),
 len=bind2nd(get,"length"),
 //mapObj=compose(curry2nd(call_([].map)),Object.keys),//function(f,a){return Object.keys(a).map(function(i){return f(a[i],i,a)})}
 merge,
 head=bind2nd(get,0),
-last,
+last,//reverse
 tail=bind2nd(call_([].slice),1),
 init=bind2nd(call_([].slice),-1),
 zip,
 zipWith,
 repeatdCombi,
 clone,
-defaultArg
+defaultArg,
+memoize
